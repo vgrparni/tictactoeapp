d1b64a93feb1d2e72c65606d41dffade462b8b4f
Parnika <govind24n@gmail.com>
2022-08-15 21:03:42 +0530

    Logging feature added

--- src/main/java/com/tictactoe/app/TictactoeappApplication.java
+++ src/main/java/com/tictactoe/app/TictactoeappApplication.java
@@ -1,12 +1,16 @@
 package com.tictactoe.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class TictactoeappApplication {
-
+	 private static final Logger log = LoggerFactory.getLogger(TictactoeappApplication.class);
 	public static void main(String[] args) {
+		log.info("----Running tictactoeapp service---- ");
 		SpringApplication.run(TictactoeappApplication.class, args);
 	}
 
