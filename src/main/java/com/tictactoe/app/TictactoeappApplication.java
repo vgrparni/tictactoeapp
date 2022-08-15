5fcbe5340e3c9d1c01decdaa36e977e9d00d21f6
Parnika <govind24n@gmail.com>
2022-08-15 22:53:53 +0530

    Updated excluding SecurityAutoConfiguration

--- src/main/java/com/tictactoe/app/TictactoeappApplication.java
+++ src/main/java/com/tictactoe/app/TictactoeappApplication.java
@@ -4,9 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 
-@SpringBootApplication(scanBasePackages = { "com.tictactoe.app.*"})
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class, scanBasePackages = { "com.tictactoe.app.*"})
 public class TictactoeappApplication {
 	 private static final Logger log = LoggerFactory.getLogger(TictactoeappApplication.class);
 	public static void main(String[] args) {
