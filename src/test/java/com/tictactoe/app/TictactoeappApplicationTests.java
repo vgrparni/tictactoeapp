1532534865f19507a87541348d239a8a36e3e119
Parnika <govind24n@gmail.com>
2022-08-15 20:31:10 +0530

    Initial commit

---
+++ src/test/java/com/tictactoe/app/TictactoeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tictactoe.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
