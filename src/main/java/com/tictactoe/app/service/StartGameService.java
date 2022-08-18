81790533deeb5993f06cedee076551f25b79a992
Parnika <govind24n@gmail.com>
2022-08-18 11:35:22 +0530

    refactor: Code refactored

--- src/main/java/com/tictactoe/app/service/StartGameService.java
+++ src/main/java/com/tictactoe/app/service/StartGameService.java
@@ -12,14 +12,13 @@ import com.tictactoe.app.openapi.model.NewGameInfo;
 
 @Service
 public class StartGameService implements TictactoeApiDelegate {
+	private String MESSAGE = "Hello Mr.X and Mr.O your game started!,All the best and enjoy playing";
 	
-	private NewGameInfo newGameInfo;
-	private  Map<String, String> board;
    @Override
    public ResponseEntity<NewGameInfo> startNewGame() {
-	   newGameInfo = new NewGameInfo();
-	   newGameInfo.setMessage("Hello Mr.X and Mr.O your game started!,All the best and enjoy playing");
-	   board = new HashMap<>();
+	   NewGameInfo newGameInfo = new NewGameInfo();
+	   newGameInfo.setMessage(MESSAGE);
+	   Map<String, String> board  = new HashMap<>();
        newGameInfo.board(board);
 	 return new ResponseEntity<NewGameInfo>(newGameInfo, HttpStatus.CREATED);
    }
