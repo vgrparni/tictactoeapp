633e6c0959dfcafbdb3caeb2c864aab4376e09d3
Parnika <govind24n@gmail.com>
2022-08-18 12:08:09 +0530

    feat: Game board positions verified

--- src/main/java/com/tictactoe/app/service/StartGameService.java
+++ src/main/java/com/tictactoe/app/service/StartGameService.java
@@ -3,6 +3,8 @@ package com.tictactoe.app.service;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import com.tictactoe.app.openapi.model.NewGameInfo;
 
 @Service
 public class StartGameService implements TictactoeApiDelegate {
+	private static final Logger log = LoggerFactory.getLogger(StartGameService.class);
 	private String MESSAGE = "Hello Mr.X and Mr.O your game started!,All the best and enjoy playing";
 	
    @Override
@@ -19,7 +22,17 @@ public class StartGameService implements TictactoeApiDelegate {
 	   NewGameInfo newGameInfo = new NewGameInfo();
 	   newGameInfo.setMessage(MESSAGE);
 	   Map<String, String> gameBoard  = new HashMap<>();
+	   gameBoard.put("1", null);
+	   gameBoard.put("2", null);
+	   gameBoard.put("3", null);
+	   gameBoard.put("4", null);
+	   gameBoard.put("5", null);
+	   gameBoard.put("6", null);
+	   gameBoard.put("7", null);
+	   gameBoard.put("8", null);
+	   gameBoard.put("9", null);
        newGameInfo.board(gameBoard);
+       log.info("New game started!.");
 	 return new ResponseEntity<NewGameInfo>(newGameInfo, HttpStatus.CREATED);
    }
 }
--- src/test/java/com/tictactoe/app/service/StartNewGameServiceTest.java
+++ src/test/java/com/tictactoe/app/service/StartNewGameServiceTest.java
@@ -1,5 +1,6 @@
 package com.tictactoe.app.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -10,6 +11,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tictactoe.app.openapi.model.NewGameInfo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,10 +25,20 @@ public class StartNewGameServiceTest {
 	private static final String NEW_GAME_INFO_PATH = "/tictactoe/startNewGame";
 	 @Autowired
 	    private MockMvc mockMvc;
-
 	    @Test
 	    public void checkNewGmeStart() throws Exception {
 	        this.mockMvc.perform(post(NEW_GAME_INFO_PATH)).andExpect(status().is(201));
 	    }
-
+        
+	    @Test
+	    public void checkGameBoardPositionsCountAndMessage() throws Exception {
+	    	RequestBuilder requestBuilder = MockMvcRequestBuilders.post(NEW_GAME_INFO_PATH);
+			MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+			String responseBody=  result.getResponse().getContentAsString();
+			ObjectMapper objectMapper = new ObjectMapper();
+			NewGameInfo gameBoardInfo = objectMapper.readValue(responseBody, NewGameInfo.class);
+			
+	        assertEquals(9, gameBoardInfo.getBoard().size());
+	        assertEquals("Hello Mr.X and Mr.O your game started!,All the best and enjoy playing", gameBoardInfo.getMessage());
+	    }
 }
