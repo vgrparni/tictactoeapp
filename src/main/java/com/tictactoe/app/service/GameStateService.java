469c74845e3a85185d4806245652bf9efa0df4ea
Parnika <govind24n@gmail.com>
2022-08-19 10:00:54 +0530

    refactor: Extracting variable to Constant class

--- src/main/java/com/tictactoe/app/service/GameStateService.java
+++ src/main/java/com/tictactoe/app/service/GameStateService.java
@@ -1,5 +1,16 @@
 package com.tictactoe.app.service;
 
+import static com.tictactoe.app.utility.ConstantsUtility.EIGHT;
+import static com.tictactoe.app.utility.ConstantsUtility.FIVE;
+import static com.tictactoe.app.utility.ConstantsUtility.FOUR;
+import static com.tictactoe.app.utility.ConstantsUtility.MESSAGE;
+import static com.tictactoe.app.utility.ConstantsUtility.NINE;
+import static com.tictactoe.app.utility.ConstantsUtility.ONE;
+import static com.tictactoe.app.utility.ConstantsUtility.SEVEN;
+import static com.tictactoe.app.utility.ConstantsUtility.SIX;
+import static com.tictactoe.app.utility.ConstantsUtility.THREE;
+import static com.tictactoe.app.utility.ConstantsUtility.TWO;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,22 +28,21 @@ import com.tictactoe.app.openapi.model.TurnResponse;
 @Service
 public class GameStateService implements TictactoeApiDelegate {
 	private static final Logger log = LoggerFactory.getLogger(GameStateService.class);
-	private String MESSAGE = "Hello Mr.X and Mr.O your game started!,All the best and enjoy playing";
 	private Map<String, String> gameBoard = new HashMap<>();
 
 	@Override
 	public ResponseEntity<NewGameInfo> startNewGame() {
 		NewGameInfo newGameInfo = new NewGameInfo();
 		newGameInfo.setMessage(MESSAGE);
-		gameBoard.put("1", null);
-		gameBoard.put("2", null);
-		gameBoard.put("3", null);
-		gameBoard.put("4", null);
-		gameBoard.put("5", null);
-		gameBoard.put("6", null);
-		gameBoard.put("7", null);
-		gameBoard.put("8", null);
-		gameBoard.put("9", null);
+		gameBoard.put(ONE, null);
+		gameBoard.put(TWO, null);
+		gameBoard.put(THREE, null);
+		gameBoard.put(FOUR, null);
+		gameBoard.put(FIVE, null);
+		gameBoard.put(SIX, null);
+		gameBoard.put(SEVEN, null);
+		gameBoard.put(EIGHT, null);
+		gameBoard.put(NINE, null);
 		newGameInfo.gameboard(gameBoard);
 		log.info("New game started!.");
 		return new ResponseEntity<NewGameInfo>(newGameInfo, HttpStatus.CREATED);
---
+++ src/main/java/com/tictactoe/app/utility/ConstantsUtility.java
@@ -0,0 +1,14 @@
+package com.tictactoe.app.utility;
+
+public class ConstantsUtility {
+	public final static String MESSAGE = "Hello Mr.X and Mr.O your game started!,All the best and enjoy playing";
+	public final static String ONE= "1";
+	public final static String TWO= "2";
+	public final static String THREE= "3";
+	public final static String FOUR= "4";
+	public final static String FIVE= "5";
+	public final static String SIX= "6";
+	public final static String SEVEN= "7";
+	public final static String EIGHT= "8";
+	public final static String NINE= "9";
+}
