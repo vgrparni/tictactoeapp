dfb69cd89b6115027da0c816a7e82b10c5db0480
Parnika <govind24n@gmail.com>
2022-08-19 23:55:29 +0530

    refactor: Variables refactor

--- src/main/java/com/tictactoe/app/utility/ConstantsUtility.java
+++ src/main/java/com/tictactoe/app/utility/ConstantsUtility.java
@@ -1,23 +1,22 @@
 package com.tictactoe.app.utility;
 
 public class ConstantsUtility {
-	public final static String MESSAGE = "Hello Mr.X and Mr.O your game started!,All the best and enjoy playing";
-	public final static String ONE= "1";
-	public final static String TWO= "2";
-	public final static String THREE= "3";
-	public final static String FOUR= "4";
-	public final static String FIVE= "5";
-	public final static String SIX= "6";
-	public final static String SEVEN= "7";
-	public final static String EIGHT= "8";
-	public final static String NINE= "9";
-	public final static String PLAYER_X="X";
-	public final static String PLAYER_O="O";
-	public final static String PLAYER_1="Player1";
-	public final static String PLAYER_2="Player2";
-	public final static String PLAYER_X_WIN_LINE="XXX";
-	public final static String PLAYER_O_WIN_LINE="OOO";
-	public final static String GAME_DRAW="draw";
-	
-	
+	public static final String MESSAGE = "Hello Mr.X and Mr.O your game started!,All the best and enjoy playing";
+	public static final String ONE = "1";
+	public static final String TWO = "2";
+	public static final String THREE = "3";
+	public static final String FOUR = "4";
+	public static final String FIVE = "5";
+	public static final String SIX = "6";
+	public static final String SEVEN = "7";
+	public static final String EIGHT = "8";
+	public static final String NINE = "9";
+	public static final String PLAYER_X = "X";
+	public static final String PLAYER_O = "O";
+	public static final String PLAYER_1 = "Player1";
+	public static final String PLAYER_2 = "Player2";
+	public static final String PLAYER_X_WIN_LINE = "XXX";
+	public static final String PLAYER_O_WIN_LINE = "OOO";
+	public static final String GAME_DRAW = "draw";
+
 }
